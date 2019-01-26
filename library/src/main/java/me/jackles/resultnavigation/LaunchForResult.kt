bcdf8421f7ab547c5d3ebe1e9e46d6039b8da681
Jack Darlington <jack.darlington@gmail.com>
2019-01-26 01:50:35 +0000

    Added no data to cancelled as it wasn't needed

--- library/src/main/java/me/jackles/resultnavigation/LaunchForResult.kt
+++ library/src/main/java/me/jackles/resultnavigation/LaunchForResult.kt
@@ -109,9 +109,9 @@ private open class BroadcastReceiverActivityLifecycleCallbacks(internal val broa
     }
 }
 
-sealed class ChoiceResult<T>(open val data: T?) {
-    data class OK<T>(override val data: T): ChoiceResult<T>(data)
-    data class Cancelled<T>(override val data: T? = null): ChoiceResult<T>(null)
+sealed class ChoiceResult<T> {
+    data class OK<T>(val data: T): ChoiceResult<T>()
+    class Cancelled<T>: ChoiceResult<T>()
 }
 
 class LaunchForResult {
